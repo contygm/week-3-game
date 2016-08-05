4e39ca387b9ce3202d37f407b01c497e53730336
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2016-08-05 15:48:13 -0400

    mechanics are all clear

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 var wins = 0;
-var lives = 0;
+var lives = 10;
 var pastGuess = [];
 var spaceHolder = "" ;
 
@@ -11,7 +11,7 @@ var StarWars = [
 	"Anakin Skywalker","Darth Vader",
 	
 	"Luke Skywalker","Mace Windu",
-	"Padmé Amidala","The Phantom Menace",
+	"Padme Amidala","The Phantom Menace",
 	"Attack of the Clones","Revenge of the Sith",
 	"A New Hope","The Empire Strikes Back",
 	"Return of the Jedi","The Force Awakens",
@@ -23,7 +23,7 @@ var StarWars = [
 	"Rebel Alliance","Death Star",
 
 	"Emperor Palpatine","Purple Lightsaber",
-	"Jedi Master Yoda","Qui-Gon Jinn",
+	"Jedi Master Yoda","Qui Gon Jinn",
 	"May the Force Be With You","The Jedi Order"
 ]
 
@@ -48,15 +48,63 @@ function printWord() {
 	document.getElementById("word").innerHTML= spaceHolder;
 };
 
+function resetWord (){
+	theWord = StarWars[Math.floor(Math.random() * 36)];
+	pastGuess = [];
+	spaceHolder = "";
+	printWord();
+	lives = 10;
+	document.getElementById("lives").innerHTML= lives + " Lives Left";
+	document.getElementById("directions").innerHTML= "You Won!";
+	document.getElementById("lastGuess").innerHTML= "You're Last Guess was: <br>";
+	document.getElementById("pastGuess").innerHTML= "You've already used these! <br>" + pastGuess;
+};
+
 function checkGuess(letter) {
+	var correct = 0;
 	for (var n = 0; n < theWord.length; n++) {
+		
 		if (letter == theWord[n].toLowerCase()){
-			spaceHolder[n] = theWord[n];
-			document.getElementById("word").innerHTML= spaceHolder;
+			console.log(spaceHolder[n]);
+			console.log(theWord[n]);
+			console.log(spaceHolder);
+			spaceHolder = spaceHolder.substr(0, n) + theWord[n] + spaceHolder.substr(n+1);
+			correct++;
+		}
+	}
+	document.getElementById("word").innerHTML= spaceHolder;
+	if (correct > 0) {return true};
+};
+
+function checkSpaceHolder (letter) {
+	for (var n = 0; n < theWord.length; n++) {
+		if (letter == spaceHolder[n]){
 			return true;
+		}
+	}	
+};
+
+function checkWin(){
+	for(var n = 0; n < spaceHolder.length; n++){
+		if (spaceHolder.includes("_") != true){
+			wins++;
+			document.getElementById("wins").innerHTML= "Wins: "+ wins;
+			resetWord();
+			console.log(theWord);
 		}
 	}
 };
+
+function updateLives(){
+	lives--;
+	if (lives < 1) {
+		document.getElementById("directions").innerHTML= "Oops! Looks like you lost! Hit a key for your next guess!";
+		resetWord();
+	} else {
+		document.getElementById("directions").innerHTML= "Nope! Try Again!";
+		document.getElementById("lives").innerHTML= lives + " Lives Left";
+	};
+}
 
 window.onload = printWord;
 console.log(theWord);
@@ -74,17 +122,23 @@ document.onkeyup = function(event) {
 		document.getElementById("directions").innerHTML= "You guessed that already! Try again.";
 	} 
 
+	else if (checkSpaceHolder(guess)) {
+		document.getElementById("directions").innerHTML= "You got that one right already!";
+	}
+
 	// check if right
 	else if (checkGuess(guess)) {
 		document.getElementById("directions").innerHTML= "You got one!";
+		checkWin();
 	}
 
 	// wrong guess
 	else {
-		document.getElementById("directions").innerHTML= "Good Guess!";
+		document.getElementById("directions").innerHTML= "Nope! Try Again!";
 		document.getElementById("lastGuess").innerHTML= "You're Last Guess was: <br>" + guess;
 		pastGuess.push(guess);
 		document.getElementById("pastGuess").innerHTML= "You've already used these! <br>" + pastGuess;
+		updateLives();
 	}
 
 
